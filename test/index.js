7e52b01b63b19d0e7e954f5e22687eda6fe9a7df
Pouya <pouya.mozafar@gmail.com>
2019-06-17 22:59:27 +0430

    Npm package initial commit

---
+++ test/index.js
@@ -0,0 +1,8 @@
+import { expect } from 'chai'
+import vuei18NTranslationChecker from '../src'
+
+describe('vuei18NTranslationChecker', function () {
+  it('should exist', function () {
+    expect(vuei18NTranslationChecker).to.exist
+  })
+})
